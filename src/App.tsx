c9de35d94ff9f3e56484cbb3dee4968aebd0c05f
Santhiyavelu <santhiyav1507@gmail.com>
2023-08-04 12:49:27 +0100

    search & navigation

--- src/App.tsx
+++ src/App.tsx
@@ -6,25 +6,101 @@
  */
 
 import React from 'react';
-import type {PropsWithChildren} from 'react';
-import {SafeAreaView, TextInput, StyleSheet, View, Text} from 'react-native';
-import MyClasssComponent from './MyClassComponent';
-import MyFuncComponent from './MyFuncComponent';
-import UserProfile from './UserProfile';
-import CustomFlex from './CustomFlex';
+import {View, Text, LogBox, StyleSheet, Button} from 'react-native';
+import {NavigationContainer} from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import CityFlatlist from './CityFlatList';
+
+const Homescreen = ({navigation}) => {
+  console.log(navigation, 'navigation');
+  return (
+    <View
+      style={{
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center',
+        backgroundColor: 'skyblue',
+      }}>
+      <CityFlatlist />
+      <Text>Home Screen</Text>
+      {/* Navigate details to the Details route with params */}
+      <Button
+        title="Go to Details"
+        onPress={() =>
+          navigation.navigate('Details', {
+            itemId: 86,
+            message: 'anything you want order here',
+          })
+        }
+      />
+    </View>
+  );
+};
+
+const DetailScreen = ({route, navigation}) => {
+  const {itemId, message} = route.params;
+  console.log(route.params, 'routerdetails');
+  return (
+    <View
+      style={{
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center',
+        backgroundColor: 'yellow',
+      }}>
+      <Text>Details Screen</Text>
+      <Text>item id: {JSON.stringify(itemId)}</Text>
+      <Text>Message: {JSON.stringify(message)}</Text>
+      <Button
+        title="Go to Details... again"
+        onPress={() =>
+          navigation.push('Details', {itemId: Math.floor(Math.random() * 100)})
+        }
+      />
+      <Button
+        title="Go back to first screen in stack"
+        onPress={() => navigation.popToTop()}
+      />
+      <Button
+        title="Go to Service Screen"
+        onPress={() => navigation.navigate('Service')}
+      />
+      <Button title="Go back" onPress={() => navigation.goBack()} />
+    </View>
+  );
+};
+
+const ServiceScreen = () => {
+  return (
+    <View
+      style={{
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center',
+        backgroundColor: 'green',
+      }}>
+      <Text>Service Screen</Text>
+    </View>
+  );
+};
+
+const Stack = createNativeStackNavigator();
 
 function App(): JSX.Element {
   return (
-    // <SafeAreaView>
-
-    <>
-      <MyClasssComponent bgcolor={'skyblue'}>
-        <Text>Embedded HOC</Text>
-      </MyClasssComponent>
-      <MyFuncComponent />
-      <CustomFlex />
-    </>
-    // </SafeAreaView>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen
+          name="Home"
+          component={Homescreen}
+          options={{title: 'Home'}}></Stack.Screen>
+        <Stack.Screen
+          name="Details"
+          component={DetailScreen}
+          options={{title: 'Details'}}></Stack.Screen>
+        <Stack.Screen name="Service" component={ServiceScreen}></Stack.Screen>
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
--- src/CityFlatList.js
+++ src/CityFlatList.js
@@ -2,6 +2,7 @@ import {useState} from 'react';
 import {
   Text,
   SafeAreaView,
+  ScrollView,
   StyleSheet,
   StatusBar,
   FlatList,
@@ -34,8 +35,9 @@ const cityData = [
 ];
 
 const CityFlatlist = () => {
-  const [getCity, setcitylist] = useState('Type city');
-  const [isModalVisible, setisModalvisible] = useState(false);
+  const [searchTerm, setSearchTerm] = useState('');
+  const [getmodalvalue, setModalvalue] = useState('');
+  const [isModalVisible, setisModalvisible] = useState(true);
   const [cityValues, setCityvalues] = useState(cityData);
 
   const toggleModal = () => {
@@ -44,44 +46,81 @@ const CityFlatlist = () => {
 
   const renderModal = () => {
     return (
-      <View style={{flex: 1}}>
-        <Button title="Show modal" onPress={toggleModal} />
-        <Modal isVisible={isModalVisible}>
-          <View style={{flex: 1}}>
-            <Text>Hello!</Text>
-            <Button title="Hide modal" onPress={toggleModal} />
-          </View>
-        </Modal>
-      </View>
+      <Modal isVisible={isModalVisible}>
+        <View>
+          <TextInput
+            placeholder="Type here to add city"
+            value={getmodalvalue}
+            autoCapitalize="none"
+            onChangeText={changedcityName => {
+              setModalvalue(changedcityName);
+            }}></TextInput>
+          <TouchableOpacity
+            style={{
+              height: 50,
+              backgroundColor: 'green',
+              justifyContent: 'center',
+              alignItems: 'center',
+              margin: 10,
+            }}
+            onPress={() => {
+              setCityvalues([...cityValues, {city: getmodalvalue}]); //Using spread operator appending the data from static cityData
+            }}>
+            <Text>Submit</Text>
+          </TouchableOpacity>
+        </View>
+
+        <View style={{flex: 1}}>
+          <Text>Hello!</Text>
+          <Button title="Hide modal" onPress={toggleModal} />
+        </View>
+      </Modal>
     );
+  };
+  const handleSearch = searchText => {
+    if (searchText) {
+      const filteredData = cityValues.filter(cityname => {
+        return cityname.city.includes(searchText);
+      });
+      console.log(filteredData, 'filtered');
+      setCityvalues(filteredData);
+    } else {
+      setCityvalues(cityData);
+    }
   };
 
   return (
     <SafeAreaView style={styles.container}>
-      <TextInput
-        value={getCity}
-        onChangeText={changedcityName => {
-          setcitylist(changedcityName);
-        }}></TextInput>
-      <TouchableOpacity
-        style={{
-          height: 50,
-          backgroundColor: 'green',
-          justifyContent: 'center',
-          alignItems: 'center',
-          margin: 10,
-        }}
-        onPress={() => {
-          setCityvalues([...cityValues, {city: getCity}]); //Using spread operator appending the data from static cityData
-        }}>
-        <Text>Submit</Text>
-      </TouchableOpacity>
-      <FlatList
-        data={cityValues}
-        renderItem={({item, index}) => (
-          <Text style={styles.cityList}>{item.city} </Text>
-        )}
-      />
+      <View>
+        <TextInput
+          style={styles.searchBox}
+          placeholder="Search Here"
+          value={searchTerm}
+          autoCapitalize="none"
+          onChangeText={changedcityName => {
+            handleSearch(changedcityName);
+            setSearchTerm(changedcityName);
+          }}></TextInput>
+        <FlatList
+          style={styles.container}
+          data={cityValues}
+          keyExtractor={item => item.id}
+          renderItem={({item, index}) => (
+            <Text style={styles.cityList}>{item.city} </Text>
+          )}
+        />
+        <Button
+          title="Show Modal"
+          style={{
+            backgroundColor: 'green',
+            borderColor: '#CCC',
+            borderWidth: 1,
+            borderRadius: 8,
+            paddingHorizontal: 20,
+          }}
+          onPress={toggleModal}
+        />
+      </View>
       {renderModal()}
     </SafeAreaView>
   );
@@ -97,10 +136,16 @@ const styles = StyleSheet.create({
     borderWidth: 2,
     borderColor: 'black',
     borderRadius: 5,
-    padding: 10,
-    margin: 10,
-    // marginHorizontal: 16,
+    padding: 5,
+    marginHorizontal: 10,
     fontSize: 20,
+  },
+  searchBox: {
+    paddingHorizontal: 10,
+    paddingVertical: 10,
+    borderColor: '#CCC',
+    borderWidth: 1,
+    borderRadius: 8,
   },
 });
 
