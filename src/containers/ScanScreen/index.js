de274fb2f90b07c4ff60e0b9c13e9c07989b726a
Santhiyavelu <santhiyav1507@gmail.com>
2023-09-08 13:40:37 +0100

    RN animation implementaion

--- src/containers/ScanScreen/index.js
+++ src/containers/ScanScreen/index.js
@@ -1,17 +1,19 @@
-import React, {useState} from 'react'; // Import useState
-import {StyleSheet, Text, TouchableOpacity, Linking, View} from 'react-native';
+import React, {useState} from 'react';
+import {
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  Linking,
+  View,
+  TouchableWithoutFeedback,
+} from 'react-native';
 import QRCodeScanner from 'react-native-qrcode-scanner';
 import {RNCamera} from 'react-native-camera';
 import LocationHelper from '../../helper/locationhelper';
+import * as Animatable from 'react-native-animatable';
 
 const ScanScreen = ({navigation}) => {
-  const [isScanning, setIsScanning] = useState(false); // Add state to track scanning
-
-  const onSuccess = e => {
-    Linking.openURL(e.data).catch(err =>
-      console.error('An error occurred', err),
-    );
-  };
+  const [isScanning, setIsScanning] = useState(false);
 
   const handleScan = async () => {
     LocationHelper.checkCameraPermission(
@@ -25,56 +27,111 @@ const ScanScreen = ({navigation}) => {
     );
   };
 
+  const onSuccess = e => {
+    Linking.openURL(e.data).catch(err =>
+      console.error('An error occurred', err),
+    );
+  };
+
   return (
-    <View>
-      <View>
-        {isScanning ? (
-          <QRCodeScanner
-            onRead={onSuccess}
-            flashMode={RNCamera.Constants.FlashMode.torch}
-            topContent={
-              <Text style={styles.centerText}>
-                Go to{' '}
-                <Text style={styles.textBold}>wikipedia.org/wiki/QR_code</Text>{' '}
-                on your computer and scan the QR code.
-              </Text>
-            }
-            bottomContent={
+    <View style={styles.container}>
+      <View style={styles.animationContainer}>
+        <Animatable.Text
+          animation="slideInDown"
+          iterationCount={10}
+          direction="alternate"
+          style={styles.animationText}>
+          Up and down you go
+        </Animatable.Text>
+        <Animatable.Text
+          animation="pulse"
+          easing="ease-out"
+          iterationCount="infinite"
+          style={styles.animationText}>
+          ❤️
+        </Animatable.Text>
+        <Animatable.View animation="rotate" ref={ref => (this.view = ref)}>
+          <Text style={styles.animationText}>Zoom me out</Text>
+        </Animatable.View>
+        <TouchableWithoutFeedback onPress={() => this.view.bounce(800)}>
+          <View>
+            <Text style={styles.animationText}>Bounce me!</Text>
+          </View>
+        </TouchableWithoutFeedback>
+      </View>
+
+      {isScanning ? (
+        <QRCodeScanner
+          onRead={onSuccess}
+          flashMode={RNCamera.Constants.FlashMode.torch}
+          topContent={
+            <Text style={styles.centerText}>
+              Go to{' '}
+              <Text style={styles.textBold}>wikipedia.org/wiki/QR_code</Text> on
+              your computer and scan the QR code.
+            </Text>
+          }
+          bottomContent={
+            <Animatable.View animation="bounceIn" delay={1000}>
               <TouchableOpacity style={styles.buttonTouchable}>
                 <Text style={styles.buttonText}>OK. Got it!</Text>
               </TouchableOpacity>
-            }
-          />
-        ) : (
-          <TouchableOpacity style={styles.buttonTouchable} onPress={handleScan}>
-            <Text style={styles.buttonText}>Scan QR Code</Text>
-          </TouchableOpacity>
-        )}
-      </View>
-      <View>
-        <TouchableOpacity onPress={() => navigation.navigate('signUp')}>
-          <Text>Go to Home</Text>
+            </Animatable.View>
+          }
+        />
+      ) : (
+        <TouchableOpacity style={styles.scanButton} onPress={handleScan}>
+          <Text style={styles.buttonText}>Scan QR Code</Text>
         </TouchableOpacity>
-      </View>
+      )}
+
+      <TouchableOpacity onPress={() => navigation.navigate('signUp')}>
+        <Text style={styles.homeLink}>Go to Home</Text>
+      </TouchableOpacity>
     </View>
   );
 };
 
 const styles = StyleSheet.create({
-  centerText: {
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5F5F5',
+  },
+  animationContainer: {
+    marginBottom: 20,
+  },
+  animationText: {
     fontSize: 18,
+    marginBottom: 10,
+    textAlign: 'center',
+    color: '#333',
+  },
+  centerText: {
+    fontSize: 16,
     color: '#777',
+    marginBottom: 20,
+    textAlign: 'center',
   },
   textBold: {
     fontWeight: '500',
     color: '#000',
   },
-  buttonTouchable: {
-    padding: 16,
+  scanButton: {
+    backgroundColor: 'rgb(0,122,255)',
+    paddingVertical: 16,
+    paddingHorizontal: 32,
+    borderRadius: 8,
   },
   buttonText: {
-    fontSize: 21,
-    color: 'rgb(0,122,255)',
+    fontSize: 18,
+    color: 'white',
+  },
+  homeLink: {
+    fontSize: 16,
+    color: 'blue',
+    marginTop: 20,
   },
 });
 
